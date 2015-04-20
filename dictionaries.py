85c51b031f16f7d36bf8aa46ef28daa3661beb6f
TechnoAyan <ping2ayan@gmail.com>
2015-04-20 12:46:02 +0530

    added dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+'''myExample = {'someItem': 2, 'otherItem':20}
+print(myExample['otherItem'])'''
+
+myExample = {'someItem': 2, 'otherItem': 20}
+myExample['newItem'] = 400
+for a in myExample:
+ print (a)
